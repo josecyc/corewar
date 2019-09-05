36ca89096533e0e95c4da1338f2aae6009893171
jcruz-y-@e1z3r3p6.42.us.org <jcruz-y-@42.us.org>
2019-09-05 11:27:52 -0700

    zezt champion testing

--- corewar42team/vm/champions/zork_playg.s
+++ corewar42team/vm/champions/zork_playg.s
@@ -1,9 +1,30 @@
-.name "zork"
-.comment "sadf"
+.name "zezting"
+.comment "da zezt"
 
-l2:	
-		sti r1, %:live, %1
-		and r1, %0, r1
 
-live:	live %1
-		lfork %2000000
+ld  %2, r2
+#ld   2, r2 
+
+ldi r1, %2, r2
+
+#and r1, 3, r2 
+#or r1, 3, r2 
+#xor r1, 3, r2 
+
+#st r1, r2 
+#st r1, 2
+
+#add r1, r2, r3
+
+#sti r1, %:live, %1
+
+#fork
+#lfork
+
+#lld
+#lldi
+
+#aff
+
+#live:	live %1
+#		lfork %2000000
