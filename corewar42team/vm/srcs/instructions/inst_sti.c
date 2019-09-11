95a7d261403b632f1f9db1c73b9074dff99f787d
jcruz-y-@e1z3r3p6.42.us.org <jcruz-y-@42.us.org>
2019-09-11 10:27:33 -0700

    sti good

--- corewar42team/vm/srcs/instructions/inst_sti.c
+++ corewar42team/vm/srcs/instructions/inst_sti.c
@@ -6,7 +6,7 @@
 /*   By: jcruz-y- <jcruz-y-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 01:22:26 by viduvern          #+#    #+#             */
-/*   Updated: 2019/09/10 16:39:19 by jcruz-y-         ###   ########.fr       */
+/*   Updated: 2019/09/11 09:55:23 by jcruz-y-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,44 @@ static void		write_data(t_player *cur, t_arena *arena)
 	}
 }
 
+int				inst_sti2(t_player *cur, t_arena *arena, int *i, int *j)
+{
+	int		result;
+
+	if ((cur->inst->ebyte >> *i & 3) == REG_CODE)
+	{
+		if (!valid_reg_int(cur, *j) && advance_proc_pc(&cur,
+jump_next_op(cur->inst->op_code)))
+			return (0);
+		cur->inst->args[*j] = cur->reg[cur->inst->args[*j] - 1];
+	}
+	else if ((cur->inst->ebyte >> *i & 3) == IND_CODE)
+	{
+		result = get_addr_value(cur, (-cur->inst->size));
+		cur->write_addr = get_addr_value(cur, \
+		(cur->inst->args[*j] % IDX_MOD) + result);
+		memory_to_int(&cur->inst->args[*j], arena, cur->write_addr, 4);
+	}
+	*i = *i - 2;
+	return (1);
+}
+
 void			inst_sti(t_player *cur, t_arena *arena)
 {
-	int a;
-	int args;
-	int result;
+	int i;
+	int j;
 
-	args = 0;
-	a = 4;
+	j = 0;
+	i = 4;
 	if (!valid_reg_int(cur, 0))
 	{
 		advance_proc_pc(&cur, jump_next_op(cur->inst->op_code));
 		return ;
 	}
-	while (a > 0 && ++args)
+	while (i > 0 && ++j)
 	{
-		if ((cur->inst->ebyte >> a & 3) == REG_CODE)
-			cur->inst->args[args] = cur->reg[cur->inst->args[args] - 1];
-		else if ((cur->inst->ebyte >> a & 3) == IND_CODE)
-		{
-			result = get_addr_value(cur, (-cur->inst->size));
-			cur->write_addr = get_addr_value(cur, \
-			(cur->inst->args[args] % IDX_MOD) + result);
-			memory_to_int(&cur->inst->args[args], arena, cur->write_addr, 4);
-		}
-		a -= 2;
+		if (!inst_sti2(cur, arena, &i, &j))
+			return ;
 	}
 	write_data(cur, arena);
 }
