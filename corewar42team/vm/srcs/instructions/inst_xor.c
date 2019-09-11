f35e3ee82f5bdbcb400b50983b96f81ff812e0ce
jcruz-y-@e1z3r3p6.42.us.org <jcruz-y-@42.us.org>
2019-09-11 12:01:28 -0700

    sti good

--- corewar42team/vm/srcs/instructions/inst_xor.c
+++ corewar42team/vm/srcs/instructions/inst_xor.c
@@ -6,7 +6,7 @@
 /*   By: jcruz-y- <jcruz-y-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 10:37:45 by viduvern          #+#    #+#             */
-/*   Updated: 2019/09/10 21:57:42 by jcruz-y-         ###   ########.fr       */
+/*   Updated: 2019/09/11 10:38:40 by jcruz-y-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 ** and stores the result in ARG_3 reg
 */
 
-void        inst_xor(t_player *cur, t_arena *arena)
+int        inst_xor_pt2(t_player *cur, t_arena *arena, int *i, int *j)
 {
 	int		addr;
 	short	byte;
